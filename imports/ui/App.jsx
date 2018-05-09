d9e95090862a6ad476601891e40286e1f807f990
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-05-09 15:43:28 -0700

    Add 'App' component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { withTracker } from 'meteor/react-meteor-data';
+// import { withTracker } from 'meteor/react-meteor-data';
 
 // App component - represents the whole app
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
   }
@@ -11,14 +11,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        Hello world!
+        <p>Hello world!</p>
       </div>
     )
   }
 }
 
-export default withTracker(() => {
-  return {
-    currentUser: Meteor.user()
-  };
-})(App);
+// export default withTracker(() => {
+//   return {};
+// })(App);
