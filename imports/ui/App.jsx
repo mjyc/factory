4b01b20bc9d4b02e82ae6cf141b883e88da2aaf2
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-05-09 16:48:34 -0700

    Initial material-ui integration

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 // import { withTracker } from 'meteor/react-meteor-data';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+import RaisedButton from 'material-ui/RaisedButton';
+
 // App component - represents the whole app
 export default class App extends Component {
   constructor(props) {
@@ -11,7 +23,42 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <p>Hello world!</p>
+        <MuiThemeProvider>
+          <Table
+            fixedHeader={true}
+            selectable={false}
+            style={{ margin: 0 }}
+          >
+            <TableHeader
+              displaySelectAll={false}
+              adjustForCheckbox={false}
+            >
+              <TableRow>
+                <TableHeaderColumn>ID</TableHeaderColumn>
+                <TableHeaderColumn>Title</TableHeaderColumn>
+                <TableHeaderColumn>Last modified</TableHeaderColumn>
+                <TableHeaderColumn>Options</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+                  <TableRow key={'program._id'} >
+                    <TableRowColumn>{'program._id'}</TableRowColumn>
+                    <TableRowColumn>{'program.title'}</TableRowColumn>
+                    <TableRowColumn>
+                    {'time'}
+                    </TableRowColumn>
+                    <TableRowColumn>
+                      <RaisedButton
+                        label='Edit'
+                      />
+                      <RaisedButton
+                        label='Delete'
+                      />
+                    </TableRowColumn>
+                  </TableRow>
+            </TableBody>
+          </Table>
+        </MuiThemeProvider>
       </div>
     )
   }
