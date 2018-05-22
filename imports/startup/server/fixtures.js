4a9e52faa4d8c4b04adc3deff21a34c96cb37968
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-05-22 11:38:17 -0700

    Refactor logger.setLevel

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,15 +1,26 @@
 import util from 'util';
 import * as log from 'loglevel';
 import { Meteor } from 'meteor/meteor';
-import {
-  Speechbubbles,
-  Speech,
-} from 'meteor/simple-face';
 
 const logger = log.getLogger('fixtures');
 
 const obj2str = (obj) => { return util.inspect(obj, true, null, true); }
 
+// Set logger levels
+if (Meteor.settings.logging) {
+  const logging = Meteor.settings.logging;
+  if (logging.default) log.setDefaultLevel(logging.default);
+  if (logging.levels) {
+    for (let name in logging.levels) {
+      log.getLogger(name).setLevel(logging.levels[name]);
+    }
+  }
+}
+
+import {
+  Speechbubbles,
+  Speech,
+} from 'meteor/simple-face';
 
 // Add and remove actions on user creation and deletion
 Meteor.users.find().observeChanges({
