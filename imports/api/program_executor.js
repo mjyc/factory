0394e2d2efd5f48095afc432eef918cd45842d8b
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-06-12 10:43:56 -0700

    WIP

--- imports/api/program_executor.js
+++ imports/api/program_executor.js
@@ -1,14 +1,14 @@
-
+import log from 'meteor/mjyc:loglevel';
 import util from 'util';
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check, Match } from 'meteor/check';
 
-import { SpeechbubbleAction } from '../api/speechbubbles.js';
-import { SpeechActions } from '../api/speech.js';
-import { MediaActions } from '../api/media.js';
-import { FacialExpressionActions } from '../api/facial_expression.js';
-import { VisionActions } from '../api/vision.js';
+// import { SpeechbubbleAction } from '../api/speechbubbles.js';
+// import { SpeechActions } from '../api/speech.js';
+// import { MediaActions } from '../api/media.js';
+// import { FacialExpressionActions } from '../api/facial_expression.js';
+// import { VisionActions } from '../api/vision.js';
 
 import { Programs } from './programs.js'
 
@@ -27,25 +27,46 @@ if (Meteor.isServer) {
   // TODO: update this
   const getSpeechbubbleActions = ({userId = ''} = {}) => {
     const actions = {
-        'robot': Speechbubbles.findOne({owner: userId, role: 'robot'}),
-        'human': Speechbubbles.findOne({owner: userId, role: 'human'}),
-        'synthesis': SpeechActions.findOne({owner: userId, type: 'synthesis'}),
-        'recognition': SpeechActions.findOne({owner: userId, type: 'recognition'}),
-        'sound': MediaActions.findOne({owner: userId}),
-        'eyes': FacialExpressionActions.findOne({owner: userId}),
-        'video': VisionActions.findOne({owner: userId, type: 'video_control'}),
-        'face': VisionActions.findOne({owner: userId, type: 'pose_detection'}),
-        'pose': VisionActions.findOne({owner: userId, type: 'face_detection'}),
-      }
-    }
+      'robot': Speechbubbles.findOne({owner: userId, role: 'robot'}),
+      'human': Speechbubbles.findOne({owner: userId, role: 'human'}),
+      'synthesis': SpeechActions.findOne({owner: userId, type: 'synthesis'}),
+      'recognition': SpeechActions.findOne({owner: userId, type: 'recognition'}),
+      'sound': MediaActions.findOne({owner: userId}),
+      'eyes': FacialExpressionActions.findOne({owner: userId}),
+      'video': VisionActions.findOne({owner: userId, type: 'video_control'}),
+      'face': VisionActions.findOne({owner: userId, type: 'pose_detection'}),
+      'pose': VisionActions.findOne({owner: userId, type: 'face_detection'}),
+    };
 
     const actionClients = {
       'robot': getActionClient(Speechbubbles, actions.robot._id),
+      'human': getActionClient(Speechbubbles, actions.human._id),
     }
 
-    const robot = {
-      displayMessage: () => { actionClient.robot.sendGoal(); return actionClient; }
-    }
+    // class Robot {
+
+
+    // }
+
+    // const robot = {
+
+    //   displayMessage: () => {
+    //     actionClient.robot.sendGoal();
+    //     return actionClient;
+    //   }
+
+    //   async waitForAllActionResults: () => {
+    //     new Error('Not implemented');
+    //   }
+
+    //   async waitForOneActionResult: () => {
+    //     new Error('Not implemented');
+    //   }
+
+    //   cancelAllActions: () => {
+    //     new Error('Not implemented');
+    //   }
+    // }
 
     return {
       displayMessageLeft: null,
