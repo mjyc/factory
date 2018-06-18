1dc6c1d460dbe399d6c9b3518a5459246d384f47
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-06-17 23:17:29 -0700

    WIP

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -7,6 +7,8 @@ import AccountsUIWrapper from '../../ui/AccountsUIWrapper.jsx'
 import MainPage from '../../ui/MainPage.jsx'
 import EditPage from '../../ui/EditPage.jsx'
 import MediaFilesPage from '../../ui/MediaFilesPage.jsx'
+import FacePage from '../../ui/FacePage.jsx'
+import VisualizerPage from '../../ui/VisualizerPage.jsx'
 
 const browserHistory = createBrowserHistory();
 
@@ -17,6 +19,7 @@ export const renderRoutes = () => (
       <Route exact path="/" component={MainPage}/>
       <Route exact path="/program/:id" component={EditPage}/>
       <Route exact path="/media_files" component={MediaFilesPage}/>
+      <Route exact path="/face" component={FacePage}/>
     </Switch>
   </Router>
 );
