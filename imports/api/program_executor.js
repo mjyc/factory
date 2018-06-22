d49520f9da869d8e0bbc18c65834d66e8fa5c491
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-06-21 21:51:52 -0700

    WIP

--- imports/api/program_executor.js
+++ imports/api/program_executor.js
@@ -2,7 +2,8 @@ import log from 'meteor/mjyc:loglevel';
 import util from 'util';
 import { Meteor } from 'meteor/meteor';
 import { check, Match } from 'meteor/check';
-import { Actions, getActionClient, Detections } from 'meteor/mjyc:action';
+import { Actions, getActionClient } from 'meteor/mjyc:action';
+import { Detections } from 'meteor/mjyc:robot-face'
 
 import { Programs } from './programs.js'
 
@@ -36,12 +37,38 @@ if (Meteor.isServer) {
         'poseDetection',
         'faceDetection',
       ].map(type => {
-        return getActionClient(Actions, Actions.findOne({owner, type})._id);
+        const actionClient = getActionClient(Actions, Actions.findOne({owner, type})._id);
+        Promise.await(actionClient.cancelGoal());
+        return actionClient;
       });
 
       logger.debug(`executing code: ${code}`);
       Meteor.defer(() => {
-        logger.info(`eval output: ${obj2str(eval(code))}`);
+        let exitcode;
+        try {
+          exitcode = eval(code);
+        } catch (err) {
+          logger.warn('code threw an error', err);
+          exitcode = 1;
+        }
+        logger.info('code finished with exitcode', exitcode);
+
+        [
+          eyeExpression,
+          soundPlay,
+          speechSynthesis,
+          speechRecognition,
+          speechbubbleRobot,
+          speechbubbleHuman,
+          videoControl,
+          poseDetection,
+          faceDetection,
+        ].map((action) => {
+          action.cancelGoal();
+        });
+        videoControl.sendGoal({type: 'stop'});
+        speechbubbleRobot.sendGoal({type: '', data: {}});
+        speechbubbleHuman.sendGoal({type: '', data: {}});
       })
     },
   });
